c5c74bba6457e9605c72bde43393dbb61211f4d0
Robert Hülsmann <robert@huelsi.de>
2019-12-17 10:49:24 +0100

    working on multi rviz windows

--- hector_sensor_proc_launch/multimesh/main.py
+++ hector_sensor_proc_launch/multimesh/main.py
@@ -2,13 +2,11 @@
 configs = ['l_i', 'l_d', 'l_c', 'n_i', 'n_d', 'n_c']
 template_path = 'enrich2019_mesh__.rviz'
 template = open(template_path, "r").read()
-print(template)
+# print(template)
 
-# input()
-#
-# for c in configs:
-#     new_content = template.replace("rnms__", "rnms_{}".format(c))
-#     print(new_content)
-#     input()
-#     with open("rnms_{}.rviz".format(c), 'w') as file:
-#         file.write(new_content)
+for c in configs:
+    new_content = template.replace("rnms__", "rnms_{}".format(c))
+    # print(new_content)
+    # input()
+    with open("enrich2019_mesh_rnms_{}.rviz".format(c), 'w') as file:
+        file.write(new_content)
